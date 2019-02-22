3afbb5c3b156bd14340464086e4698dee73d8e37
Jamling <li.jamling@gmail.com>
2019-02-22 15:38:45 +0800

    mv file storage

--- core/src/main/java/cn/ieclipse/util/LimitArrayList.java
+++ core/src/main/java/cn/ieclipse/util/LimitArrayList.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package cn.ieclipse.smartim.helper;
+package cn.ieclipse.util;
 
 import java.util.ArrayList;
 import java.util.Collection;
